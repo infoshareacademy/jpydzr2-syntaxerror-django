caa4214eb51f1c179da4612093d77c6ef4b5eaf9
Mateusz <m.zajac1988@gmail.com>
2021-05-05 18:09:31 +0200

    predict based on the user input from the page

--- syntaxerror_project/credit_project/asgi.py
+++ syntaxerror_project/credit_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for credit_project project.
+ASGI config for syntaxerror_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'credit_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syntaxerror_project.settings')
 
 application = get_asgi_application()
