8727b1c2a83d2cac3b64ad0ac65d05f6ae7153ca
Mateusz <m.zajac1988@gmail.com>
2021-05-17 16:23:03 +0200

    add prediction model

--- syntaxerror_project/credit_app/admin.py
+++ syntaxerror_project/credit_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from credit_app.models import PredictModel
+
 
 # Register your models here.
+
+@admin.register(PredictModel)
+class PredictModelAdmin(admin.ModelAdmin):
+
+    list_display = [
+        '__str__',
+        'married',
+        'education',
+        'applicant_income',
+        'co_applicant_income',
+        'loan_amount',
+        'loan_term',
+        'credit_history',
+        'loan_chances',
+    ]
--- syntaxerror_project/credit_app/models.py
+++ syntaxerror_project/credit_app/models.py
@@ -1,2 +1,23 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
+
+# Create your models here.
+class PredictModel(models.Model):
+    requestor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    married = models.IntegerField()
+    education = models.IntegerField()
+    applicant_income = models.IntegerField()
+    co_applicant_income = models.IntegerField()
+    loan_amount = models.IntegerField()
+    loan_term = models.IntegerField()
+    credit_history = models.IntegerField()
+    loan_chances = models.DecimalField(decimal_places=2, max_digits=2)
+
+    def __str__(self):
+        return '{}'.format(self.requestor)
 
