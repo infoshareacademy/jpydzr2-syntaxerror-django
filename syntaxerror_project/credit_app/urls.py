fa1297216f5cb366af6c436ff21011f57f08ac08
Mateusz <m.zajac1988@gmail.com>
2021-05-22 20:07:10 +0200

    ostylowane strony logowania i rejestracji

--- syntaxerror_project/credit_app/urls.py
+++ syntaxerror_project/credit_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'credit_app'
@@ -6,4 +8,4 @@ app_name = 'credit_app'
 urlpatterns = [
     path('predict/', views.predict_chances, name='submit_prediction'),
     path('requests/', views.RequestsView.as_view(), name='requests'),
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- syntaxerror_project/credit_project/settings.py
+++ syntaxerror_project/credit_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'apps_static/')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/static/images',
+    '/static/styles',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
