73371eb36940b547dca920c4edfc65cd4063b93c
Mateusz <m.zajac1988@gmail.com>
2021-05-27 21:47:02 +0200

    better alignment and responsiveness

--- syntaxerror_project/static/js/scripts.js
+++ syntaxerror_project/static/js/scripts.js
@@ -1,26 +1,26 @@
 // Datatables
-$(document).ready(function() {
+$(document).ready(function () {
     $('#results_table').DataTable();
-} );
+});
 
 // Change nav color on scroll
 $(function () {
-  $(document).scroll(function () {
-    var $nav = $("#main-nav");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
+    $(document).scroll(function () {
+        var $nav = $("#main-nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
 });
 
 // Remove hashes from the page when using #anchor links
-$(document).ready(function() {
-  const menuBtn = $('.hack15-menu-button');
-  menuBtn.click(()=>{
-    setTimeout(()=>{
-      removeHash();
-    }, 5);
-  });
+$(document).ready(function () {
+    const menuBtn = $('.hack15-menu-button');
+    menuBtn.click(() => {
+        setTimeout(() => {
+            removeHash();
+        }, 5);
+    });
 
-  function removeHash(){
-    history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
-  }
+    function removeHash() {
+        history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+    }
 });
