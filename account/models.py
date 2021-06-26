3b84f2b7a59dbf8ddc41c02f42d94a71be0b01be
Mateusz <m.zajac1988@gmail.com>
2021-06-26 10:02:19 +0200

    add user profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    married = models.IntegerField(blank=True, null=True)
+    education = models.IntegerField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    phone_num = models.CharField(max_length=16, blank=True, null=True)
+    street = models.CharField(max_length=255, blank=True, null=True)
+    str_building_no = models.CharField(max_length=10, blank=True, null=True)
+    str_local_no = models.CharField(max_length=10, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    zip_code = models.CharField(max_length=10, blank=True, null=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
