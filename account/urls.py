0eaf1460ff7418f110280ecab8312d57e9e658aa
Mateusz <m.zajac1988@gmail.com>
2021-07-10 06:50:23 +0200

    add testimonials pic

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
     path('edit/', views.update_profile, name='edit'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
